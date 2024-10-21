ef0689bf0e3df214a51834885b05b13ff3c52925
Danesh <techydanesh@gmail.com>
2024-10-21 17:00:30 +0530

    login route and handeler created

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,14 @@
 const userModel = require("../models/user-model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const {generateToken} = require("../utils/generateToken")
+const { generateToken } = require("../utils/generateToken");
 
 module.exports.registerUser = async (req, res) => {
   try {
     let { email, fullname, password } = req.body;
 
-    let user = await userModel.findOne({email: email})
-    if(user) return res.status(400).send("user already exist's")
+    let user = await userModel.findOne({ email: email });
+    if (user) return res.status(400).send("user already exist's");
     bcrypt.genSalt(10, function (err, salt) {
       bcrypt.hash(password, salt, async function (err, hash) {
         if (err) return res.send(err.message);
@@ -27,4 +27,20 @@ module.exports.registerUser = async (req, res) => {
   } catch (err) {
     res.send(err.message);
   }
+};
+
+module.exports.loginUser = async (req, res) => {
+  let { email, password } = req.body;
+
+  let user = await userModel.findOne({ email: email });
+  if (!user) return res.status(401).send("email or password wrong !!");
+
+  bcrypt.compare(password, user.password, function (err, result) {
+    if (result) {
+      let token = generateToken(user);
+      res.cookie("token", token).status(200).send("you can login !");
+    }else{
+      return res.status(401).send("email or password wrong !!");
+    }
+  });
 };
