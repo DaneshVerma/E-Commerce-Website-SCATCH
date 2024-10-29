4ce03f3c8d3949286c4e59da440c4054fcbd60ac
Danesh <techydanesh@gmail.com>
2024-10-29 13:16:48 +0530

    added  cart property to userModel shcema and creted a addtocart post reqruest handler

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,24 @@ const express = require("express");
 const isLoggedIn = require("../middleware/isLoggedIn");
 const router = express.Router();
 const productModel = require("../models/product-model");
+const userModel = require("../models/user-model");
 
 router.get("/", (req, res) => {
   let error = req.flash("error");
-  res.render("index", { error });
+  res.render("index", { error, loggedIn: false });
 });
 
 router.get("/shop", isLoggedIn, async (req, res) => {
-  let products = await productModel.find()
-  res.render("shop", {products});
+  let products = await productModel.find();
+  let success = req.flash("success");
+  res.render("shop", { products, success });
 });
 
+router.get("/addtocart/:productid", isLoggedIn, async (req, res) => {
+  let user = await userModel.findOne({ email: req.user.email });
+  user.cart.push(req.params.productid);
+  await user.save();
+  req.flash("success", "added to cart");
+  res.redirect("/shop");
+});
 module.exports = router;
